b6fa279922bfdbd605481791d78e20db1224fe0f
Perfspec <lewisrwebb@gmail.com>
2018-12-13 15:27:21 +0000

    new functionality added

---
+++ cv-trainer-service/src/test/java/com/qa/cvtrainerservice/CvTrainerServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qa.cvtrainerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CvTrainerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
