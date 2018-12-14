74cfe3cfde5563ec53a6998fea093190ddc76eff
Perfspec <lewisrwebb@gmail.com>
2018-12-14 12:45:50 +0000

    trainee methods added

--- cv-api-for-cloud/src/main/java/com/qa/cvapi/rest/CVEndpoint.java
+++ cv-api-for-cloud/src/main/java/com/qa/cvapi/rest/CVEndpoint.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import com.qa.cvapi.persistence.domain.CV;
 import com.qa.cvapi.service.CVService;
 
 @RestController
+@RequestMapping(path = Constants.CV_PATH_EP)
 public class CVEndpoint {
 
 	@Autowired
