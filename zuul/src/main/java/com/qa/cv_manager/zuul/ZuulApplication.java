1e2e9aa9476f5a2c08712da620fcd6ccc0c09466
Bentom08 <bentom06@hotmail.co.uk>
2018-12-19 17:08:35 +0000

    added CORS fix

--- zuul/src/main/java/com/qa/cv_manager/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/qa/cv_manager/zuul/ZuulApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.qa.cv_manager.zuul.constants.Constants;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,5 +20,19 @@ public class ZuulApplication {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin(Constants.ALL);
+	    config.addAllowedHeader(Constants.ALL);
+	    config.addAllowedMethod(Constants.GET);
+	    config.addAllowedMethod(Constants.PUT);
+	    config.addAllowedMethod(Constants.POST);
+	    config.addAllowedMethod(Constants.DELETE);
+	    source.registerCorsConfiguration(Constants.ALL_DIRS, config);
+	    return new CorsFilter(source);
+	}
 }
 
