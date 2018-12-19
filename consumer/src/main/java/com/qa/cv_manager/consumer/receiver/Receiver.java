1fc7e03431af8fde50edd3afc3458de01faa7ede
Bentom08 <bentom06@hotmail.co.uk>
2018-12-19 11:07:23 +0000

    constant

--- consumer/src/main/java/com/qa/cv_manager/consumer/receiver/Receiver.java
+++ consumer/src/main/java/com/qa/cv_manager/consumer/receiver/Receiver.java
@@ -18,12 +18,12 @@ public class Receiver {
 	@Autowired
 	private UserRepository userRepo;
 	
-	@JmsListener(destination = "${queue.saveUser}", containerFactory = "myFactory")
+	@JmsListener(destination = "${queue.saveUser}", containerFactory = "${factory.myfactory}")
 	public void receiveUsers(UserPOJO user) {
 		userRepo.save(user);
 	}
 	
-	@JmsListener(destination = "${queue.saveCV}", containerFactory = "myFactory")
+	@JmsListener(destination = "${queue.saveCV}", containerFactory = "${factory.myfactory}")
 	public void receiveUsers(CV cv) {
 		cvRepo.save(cv);
 	}
