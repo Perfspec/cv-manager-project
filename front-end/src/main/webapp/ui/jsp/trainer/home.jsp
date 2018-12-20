90333149c5505a905c99412edb16e8877c3ca7b5
alvinjo <alvin.joseph0@gmail.com>
2018-12-20 12:18:59 +0000

    trainer front end complete

--- front-end/src/main/webapp/ui/jsp/trainer/home.jsp
+++ front-end/src/main/webapp/ui/jsp/trainer/home.jsp
@@ -11,11 +11,21 @@
 <body >
        <security:authorize access="hasRole('ROLE_TRAINER')">
         <h1>Trainer Page</h1>
+         <a type="button" class="btn btn-dark btn-md"
+								style="margin-right: -20%"
+								href="<c:url value="/perform_logout" />"> <span
+								class="glyphicon glyphicon-log-out"></span> &nbsp Logout
+							</a>
+    <p></p>
     </security:authorize>
     
-    <button onclick="getAllCVs()">Get All CVs</button>
+   
+    <button onclick="getAllCVs()">Show All CVs</button>
+    <p></p>
     <button onclick="getAllFlaggedCVs()">Show All Flagged CVs</button>
+    <p></p>
     <button onclick="getAllMediumFlaggedCVs()">Show All Medium Flagged CVs</button>
+    <p></p>
     <button onclick="getAllBadFlaggedCVs()">Show All Bad Flagged CVs</button>
     <p></p>
     <div id="cvList"></div>
--- front-end/src/main/webapp/ui/static/trainer.js
+++ front-end/src/main/webapp/ui/static/trainer.js
@@ -7,6 +7,7 @@ getAllCVs = function() {
 		const jsonData = JSON.parse(Http.responseText);
 		
 		showCVs(jsonData);
+		
 		}
 		
 		Http.open("GET", url);
@@ -14,21 +15,64 @@ getAllCVs = function() {
 		
 		function showCVs(data) {
 			var output = "<table>";
-			output += "<tr>" + "<th>ID</th>" + "<th>CV</th>" + "<th>Flag</th>" + "</tr>";
+			output += "<tr>" + "<th>CVs</th>" + "<th>Flag</th>" + "<th>Change Flag</th>" + "</tr>";
 			var i;
 			
 			for (var i in data) {
-				output += "<tr>" + "<td>" + data[i].id + "</td>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].cvPath + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
+				output += "<tr>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].username + "'s CV" + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "<td>" + "<button onclick='removeFlag(" + data[i].id + ")'>Remove Flag</button>"+ "<button onclick='updateMediumFlag(" + data[i].id + ")'>Flag: Medium</button>"+ "<button onclick='updateBadFlag(" + data[i].id + ")'>Flag: Bad</button>" + "</td>" + "</tr>";
+
 			}
 			
 			output += "</table>";
 				
 				document.getElementById("cvList").innerHTML = output;
-		}
-		
+		}		
 	}
+
+updateMediumFlag= function(id) {
+	console.log(id);
+	const http = new XMLHttpRequest();
+	const url = 'http://localhost:8086/cv/updateFlag';
+		
+	const param1 = id;
+	const param2 = 1;
+	
+	console.log(url + "/" + param1 + "/" + param2);
+	
+	http.open("PUT", url + "/" + param1 + "/" + param2, true);
+	http.send();
+}
+
+updateBadFlag= function(id) {
+	console.log(id);
+	const http = new XMLHttpRequest();
+	const url = 'http://localhost:8086/cv/updateFlag';
+		
+	const param1 = id;
+	const param2 = 2;
+	
+	console.log(url + "/" + param1 + "/" + param2);
+	
+	http.open("PUT", url + "/" + param1 + "/" + param2, true);
+	http.send();
+}
+
+removeFlag= function(id) {
+	console.log(id);
+	const http = new XMLHttpRequest();
+	const url = 'http://localhost:8086/cv/updateFlag';
+		
+	const param1 = id;
+	const param2 = 0;
+	
+	console.log(url + "/" + param1 + "/" + param2);
+	
+	http.open("PUT", url + "/" + param1 + "/" + param2, true);
+	http.send();
+}
 	
 getAllFlaggedCVs = function() {
+	
 	const Http = new XMLHttpRequest();
 	const url='http://localhost:8086/cv/getAllFlagged';
 	
@@ -44,11 +88,11 @@ getAllFlaggedCVs = function() {
 		
 		function showCVs(data) {
 			var output = "<table>";
-			output += "<tr>" + "<th>User</th>" + "<th>CV</th>" + "<th>Flag</th>" + "</tr>";
+			output += "<tr>" + "<th>CVs</th>" + "<th>Flag</th>" + "</tr>";
 			var i;
 			
 			for (var i in data) {
-				output += "<tr>" + "<td>" + data[i].id + "</td>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].cvPath + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
+				output += "<tr>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].username + "'s CV" + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
 			}
 			
 			output += "</table>";
@@ -73,11 +117,11 @@ getAllMediumFlaggedCVs = function() {
 			
 			function showCVs(data) {
 				var output = "<table>";
-				output += "<tr>" + "<th>User</th>" + "<th>CV</th>" + "<th>Flag</th>" + "</tr>";
+				output += "<tr>" + "<th>CVs</th>" + "<th>Flag</th>" + "</tr>";
 				var i;
 				
 				for (var i in data) {
-					output += "<tr>" + "<td>" + data[i].id + "</td>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].cvPath + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
+					output += "<tr>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].username + "'s CV" + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
 				}
 				
 				output += "</table>";
@@ -103,11 +147,11 @@ getAllBadFlaggedCVs = function() {
 		
 		function showCVs(data) {
 			var output = "<table>";
-			output += "<tr>" + "<th>User</th>" + "<th>CV</th>" + "<th>Flag</th>" + "</tr>";
+			output += "<tr>" + "<th>CVs</th>" + "<th>Flag</th>" + "</tr>";
 			var i;
 			
 			for (var i in data) {
-				output += "<tr>" + "<td>" + data[i].id + "</td>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].cvPath + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
+				output += "<tr>" + "<td>" + "<a href='" + data[i].cvPath + "'>" + data[i].username + "'s CV" + "</a>" + "</td>" + "<td>" + data[i].flag + "</td>" + "</tr>";
 			}
 			
 			output += "</table>";
@@ -116,3 +160,13 @@ getAllBadFlaggedCVs = function() {
 		}
 		
 	}
+
+
+
+
+
+
+
+
+
+
